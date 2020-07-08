9dd8041a6277f970175ff49a861c6e5617a34ebb
kbanerje <banerjee.krishnendu@gmail.com>
2020-07-09 00:05:46 +0100

    Added support for docker push

--- build_docker_hub.sh
+++ build_docker_hub.sh
@@ -1,2 +1,4 @@
 docker build -f utils/Dockerfile.ui --tag=xprotech/country_ui .
 docker build -f utils/Dockerfile.local --tag=xprotech/country_api .
+docker push xprotech/country_api
+docker push xprotech/country_ui
